ccc09cb4d0041870750e7762d8b0d5abce37df17
Louis Marti <lmarti13@gmail.com>
2016-01-13 16:01:50 -0500

    Fixed syntax error.

--- doWork.sh
+++ doWork.sh
@@ -2,6 +2,7 @@
 mkdir Data/$1
 
 for alpha in .1 .2 .3 .4 .5 .6 .7 .8 .9
+do
 for time in {1..23}
 do
     mpirun -np 2 python ProbeSpace.py --out "Data/"$1"/"$1"_"$time"_"$alpha".pkl" --condition $1 --time $time --alpha $alpha &
