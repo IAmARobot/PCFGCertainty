be8ad14fd7b12ec388b8c54efb479121cbe0ac32
Louis Marti <lmarti13@gmail.com>
2016-01-13 14:52:08 -0500

    Added in alpha variance.

--- doWork.sh
+++ doWork.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
 mkdir Data/$1
+
+for alpha in {}
 for time in {1..23}
 do
-    mpirun -np 2 python ProbeSpace.py --out "Data/"$1"/"$1"_"$time".pkl" --condition $1 --time $time &
+    mpirun -np 2 python ProbeSpace.py --out "Data/"$1"/"$1"_"$time".pkl" --condition $1 --time $time & --alpha $alpha
 done
-mpirun -np 2 python ProbeSpace.py --out "Data/"$1"/"$1"_24.pkl" --condition $1 --time 24
+
+mpirun -np 2 python ProbeSpace.py --out "Data/"$1"/"$1"_24.pkl" --condition $1 --time 24 --alpha $alpha
+
+done
