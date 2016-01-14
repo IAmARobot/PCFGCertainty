b395f2302e5865fce5424286a5e6dd6a6f60393b
Louis Marti <lmarti13@gmail.com>
2016-01-14 10:12:32 -0500

    LALALALALALALALA

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 mkdir Data/$1
 
-for alpha in .1 .2 .3 .4 .5 .6 .7 .8 .9
+for condition in "condition1" "condition2" "condition3" "condition4" "condition5" "condition6" "condition7" "condition8" "condition9" "condition10"
 do
-    python CertaintyAnalysis.py --read "Data/"$1"/" --write "Data/"$1"/"$1"_"$alpha".csv" --condition $1 --alpha $alpha &
+    for alpha in .1 .2 .3 .4 .5 .6 .7 .8 .9
+    do
+        python CertaintyAnalysis.py --read "Data/"$condition"/" --write "Data/"$condition"/"$condition"_"$alpha".csv" --condition $condition --alpha $alpha &
+    done
 done
