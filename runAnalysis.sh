fcfba1b0476b303e635c285795be3a80cc7bc4b1
Louis Marti <lmarti13@gmail.com>
2016-01-14 10:53:27 -0500

    LALALALALALALALA

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 mkdir Data/$1
+rm Data/allConditions.csv
 
 for condition in "condition1" "condition2" "condition3" "condition4" "condition5" "condition6" "condition7" "condition8" "condition9" "condition10"
 do
