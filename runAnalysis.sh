c9cc8e0b0984eafe35c7f6488f16df4e4d24dd46
Louis Marti <lmarti13@gmail.com>
2016-01-14 10:08:35 -0500

    LALALALALALALALA

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -3,5 +3,5 @@ mkdir Data/$1
 
 for alpha in .1 .2 .3 .4 .5 .6 .7 .8 .9
 do
-    mpirun -np 2 python CertaintyAnalysis.py --read "Data/"$1"/" --write "Data/"$1"/"$1"_"$alpha".csv" --condition $1 --alpha $alpha &
+    python CertaintyAnalysis.py --read "Data/"$1"/" --write "Data/"$1"/"$1"_"$alpha".csv" --condition $1 --alpha $alpha &
 done
