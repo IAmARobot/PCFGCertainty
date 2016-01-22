408132d4f70ecf3a430ba4b5ae8c981fcfba6893
Louis Marti <lmarti13@gmail.com>
2016-01-21 23:09:07 -0500

    LALALALALALALALA

--- BestFit.py
+++ BestFit.py
@@ -11,7 +11,7 @@ import math
 from collections import defaultdict
 
 hypothesis_space = defaultdict(lambda: [])
-data = defaultdict(lambda: [defaultdict(lambda: [])])
+data = defaultdict(lambda : defaultdict(int))
 
 # Populate hypothesis space for each condition
 for condition in xrange(1, 11):
