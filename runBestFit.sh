70fd217e7419945fb14f5b86788dbe1faf74459c
Louis Marti <lmarti13@gmail.com>
2016-01-23 18:05:31 -0500

    Fixed

--- runBestFit.sh
+++ runBestFit.sh
@@ -4,6 +4,6 @@ for alpha in .01 .02 .03 .04 .05 .06 .07 .08 .09 .10 .11 .12 .13 .14 .15 .16 .17
 do
     for beta in .01 .02 .03 .04 .05 .06 .07 .08 .09 .10 .11 .12 .13 .14 .15 .16 .17 .18 .19 .20
     do
-        mpirun -np 2 python BestFit.py --alpha $alpha &
+        mpirun -np 2 python BestFit.py --alpha $alpha --beta $beta &
     done
 done
