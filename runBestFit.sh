b2367530e8ca4a11c5cc743f984279c75b240c35
Louis Marti <lmarti13@gmail.com>
2016-01-23 01:35:07 -0500

    z

--- runBestFit.sh
+++ runBestFit.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-for alpha in .1
+for alpha in .1 .2 .3 .4 .5 .6 .7 .8 .9 1
 do
     for beta in .5 1 1.5 2 2.5 3 3.5 4 4.5 5
     do
         mpirun -np 2 python BestFit.py --alpha $alpha --beta $beta &
     done
+
+    sleep 1h
 done
