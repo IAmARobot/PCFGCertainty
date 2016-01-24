63d43cd7396ae8bfc9f53647fa92065fd6b6cd66
Louis Marti <lmarti13@gmail.com>
2016-01-24 10:36:39 -0500

    Fixed

--- doWork.sh
+++ doWork.sh
@@ -2,7 +2,7 @@
 
 for condition in "condition1" "condition2" "condition3" "condition4" "condition5" "condition6" "condition7" "condition8" "condition9" "condition10"
 do
-    for time in {1..24}
+    for time in {0..23}
     do
         mpirun -np 2 python ProbeSpace.py --out "Data/"$condition"/"$condition"_"$time".pkl" --condition $condition --time $time &
     done
