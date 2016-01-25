48466b86528a9fe3f08562ccae2d955881c22e97
Louis Marti <lmarti13@gmail.com>
2016-01-25 10:01:55 -0500

    Fixed

--- runBestFit.sh
+++ runBestFit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for alpha in .41 .42 .43 .44 .45 .46 .47 .48 .49
+for alpha in .05 .1 .15 .2 .25 .3 .35 .4 .45 .5 .55 .6 .65 .7 .75 .8 .85 .9 .95 1
 do
     mpirun -np 2 python BestFit.py --alpha $alpha &
 done
