f6344376aaeef77a5eb09c3953cb24ee4b22b07e
Louis Marti <lmarti13@gmail.com>
2016-05-23 22:17:05 -0400

    Added one shot checks

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -3,5 +3,5 @@ rm modelData.csv
 
 for condition in 1 2 3 4 5 6 7 8 9 10
 do
-    mpirun -np 2 python CertaintyAnalysis.py --condition $condition &
+    mpirun -np 2 python CertaintyAnalysis.py -s --condition $condition &
 done
--- runBestFit.sh
+++ runBestFit.sh
@@ -2,5 +2,5 @@
 
 for alpha in .60 .605 .610 .620 .625 .630 .635 .640 .645 .650 .655 .660 .665 .670 .675 .680 .685 .690
 do
-    mpirun -np 2 python BestFit.py --alpha $alpha &
+    mpirun -np 2 python BestFit.py -s --alpha $alpha &
 done
