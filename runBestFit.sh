24304d029b958c1c219c5f4b50ada4d0ca7018c2
Louis Marti <lmarti13@gmail.com>
2016-05-28 21:23:39 -0400

    .

--- runBestFit.sh
+++ runBestFit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for alpha in .1 .2 .3 .4 .5 .6 .7 .8 .9 1
+for alpha in .65 .66 .67 .68 .69 .7 .71 .72 .73 .74 .75 .76 .77 .78 .79
 do
     mpirun -np 2 python BestFit.py -s --alpha $alpha &
 done
