cc365d0b385e933aca2e163768e639ff3e2b3e6b
Louis Marti <lmarti13@gmail.com>
2017-05-04 10:34:39 -0400

    .

--- Grammar.py
+++ Grammar.py
@@ -2,19 +2,27 @@ from LOTlib.Grammar import Grammar
 
 grammar = Grammar()
 
-# shape, color, size
 # DATA = image e.g., RED_TRIANGLE_LARGE
 # Hypothesis: function(image) = Bool
 
-grammar.add_rule('START', '', ['PREDICATE'], 1.0)
+#grammar.add_rule('START', 'True', None, 1.0)
+#grammar.add_rule('START', 'False', None, 1.0)
 
-grammar.add_rule('START', 'True', None, 1.0)
-grammar.add_rule('START', 'False', None, 1.0)
+grammar.add_rule('START', '', ['DISJ'], 1.0)
+
+grammar.add_rule('DISJ', '', ['CONJ'], 1.0)
+grammar.add_rule('DISJ', 'or_', ['CONJ', 'DISJ'], 1.0)
+
+grammar.add_rule('CONJ', '', ['BOOL'], 1.0)
+grammar.add_rule('CONJ', 'and_', ['BOOL', 'CONJ'], 1.0)
+
+grammar.add_rule('BOOL', '', ['PREDICATE'], 1.0)
+grammar.add_rule('BOOL', 'not_', ['PREDICATE'], 1.0)
 
 # Logical Primitives
-grammar.add_rule('PREDICATE', 'and_', ['PREDICATE', 'PREDICATE'], 1.0)
-grammar.add_rule('PREDICATE', 'or_', ['PREDICATE', 'PREDICATE'], 1.0)
-grammar.add_rule('PREDICATE', 'not_', ['PREDICATE'], 1.0)
+#grammar.add_rule('PREDICATE', 'and_', ['PREDICATE', 'PREDICATE'], 1.0)
+#grammar.add_rule('PREDICATE', 'or_', ['PREDICATE', 'PREDICATE'], 1.0)
+#grammar.add_rule('PREDICATE', 'not_', ['PREDICATE'], 1.0)
 
 # Color Primitives
 grammar.add_rule('PREDICATE', 'red_', ['IMG'], 1.0)
